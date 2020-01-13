c033f40a7c29846db448e3f3d89c0ef1cc8b1acd
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2020-01-13 20:59:48 +0800

    GCD
    euclidean algorithm

---
+++ 11417.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+// Use Euclidean Algorithm to find GCD (greatest common divisor)
+int GCD(int a, int b){
+    if (a == 0 && b!= 0){
+        return a;
+    }
+    if (b == 0){
+        return a;
+    }
+    else{
+        return GCD(b, a%b);
+    }
+}
+
+int main() {
+    int n = 0;
+    while(cin >> n){
+        if (n == 0){
+            break;
+        }
+        int g = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n ; j++) {
+                g += GCD(i,j);
+            }
+        }
+        cout << g << endl;
+    }
+    return 0;
+}
