56a0013213a3056d832d2ab21fad3f07f3323097
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2020-05-20 16:57:28 +0800

    Love Calculator
    with isalpha version

---
+++ 10424_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int char2num(string str){
+    int value = 0;
+    for (int i = 0; i < str.length(); i++) {
+        if ( isalpha(str[i]) ){
+            value += (int)tolower(str[i])- (int)'a' + 1;
+        }
+    }
+    while (value >= 10){
+        value = value / 10 + value % 10;
+    }
+    return value;
+}
+
+int main() {
+    string s1;
+    string s2;
+    while(getline(cin, s1)){
+        getline(cin, s2);
+        for (int i = 0; i < s1.length(); i++) {
+            s1[i] = tolower(s1[i]);
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            s2[i] = tolower(s2[i]);
+        }
+        double value1 = char2num(s1);
+        double value2 = char2num(s2);
+        printf("%.2lf %%\n", min(value1, value2) / max(value1, value2) * 100);
+    }
+    return 0;
+}
