d351a9434c1092d8ff093126029dc3060904b401
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2020-09-24 11:52:54 +0800

    Create A_Air_Conditioner.cpp

---
+++ Atcoder/Beginner_Contest_174/A_Air_Conditioner.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int temp;
+    cin >> temp;
+    if (temp >= 30){
+        cout << "Yes" << endl;
+    }else {
+        cout << "No" << endl;
+    }
+    return 0;
+}
