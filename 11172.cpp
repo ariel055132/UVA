1ceaa5b925b82d901ccf29bccbdf74b0f16791a6
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2020-10-12 19:50:28 +0800

    Relational Operators

---
+++ 11172.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    long long a, b;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        cin >> a >> b;
+        if (a < b) {
+            cout << "<" << endl;
+        } else if (a > b) {
+            cout << ">" << endl;
+        } else if (a == b) {
+            cout << "=" << endl;
+        }
+    }
+    return 0;
+}
