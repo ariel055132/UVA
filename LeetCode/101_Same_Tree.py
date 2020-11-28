863c27d46efe0e04f6e35176441949fddb98f3df
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2020-11-28 17:40:39 +0800

    Changes

--- LeetCode/101_Same_Tree.py
+++ LeetCode/101_Same_Tree.py
@@ -1,7 +1,15 @@
 class Solution:
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        if not p and not q: return True
-        if not p or not q: return False
-        l = self.isSameTree(p.left, q.left)
-        r = self.isSameTree(p.right, q.right)
-        return l and r and p.val == q.val
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root: 
+            return True
+        else:
+            return self.isSym(root.left, root.right)
+    
+    def isSym(self, l: TreeNode, r: TreeNode) -> bool:
+        if not l and not r:
+            return True
+        if not l or not r:
+            return False
+        if l.val != r.val:
+            return False
+        return self.isSym(l.left, r.right) and self.isSym(l.right, r.left)
