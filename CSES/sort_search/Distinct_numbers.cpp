1e100d23f84babf4ca4465df3f568c1b113ed8ef
ariel055132 <36882230+ariel055132@users.noreply.github.com>
2021-01-02 23:19:38 +0800

    Distinct numbers is added

---
+++ CSES/sort_search/Distinct_numbers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    int no;
+    vector<int> num;
+    int ans = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> no;
+        num.push_back(no);
+    }
+    sort(num.begin(), num.end());
+    for (int i = 1; i <= num.size(); i++)
+    {
+        if (num[i] != num[i-1])
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    /*
+    for (int i = 0; i < num.size(); i++)
+    {
+        cout << num[i] << endl;
+    }
+    */
+    return 0;
+}
